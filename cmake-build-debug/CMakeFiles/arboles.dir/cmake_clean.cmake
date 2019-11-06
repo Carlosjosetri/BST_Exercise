470bf23f6f7e3841ca7583fd0e581b457c447aa8
carlosjosetri <00082217@uca.edu.sv>
2019-11-06 14:27:35 -0600

    initial

---
+++ cmake-build-debug/CMakeFiles/arboles.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arboles.dir/main.cpp.obj"
+  "arboles.pdb"
+  "arboles.exe"
+  "arboles.exe.manifest"
+  "libarboles.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/arboles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
